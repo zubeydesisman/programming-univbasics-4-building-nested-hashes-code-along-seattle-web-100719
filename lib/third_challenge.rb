e2f7c7587176b2e8144ab0c7f13baac5e4fd62e1
maxwellbenton <maxwellbenton@gmail.com>
2019-04-16 21:45:12 -0400

    fixes name error

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -9,7 +9,7 @@ def third_challenge
     :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
-      :heroine => {name: "Juliette", age: "15", status: "alive"},
+      :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => []
     }
   }
