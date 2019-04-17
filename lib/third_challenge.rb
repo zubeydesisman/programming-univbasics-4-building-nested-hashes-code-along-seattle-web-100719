97dae631b629537c59490ea68d8504b32ee904d7
maxwellbenton <maxwellbenton@gmail.com>
2019-04-16 21:40:02 -0400

    clears empty lines from solution

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -1,17 +1,16 @@
 def third_challenge
- 
   epic_tragedy = {
-   :montague => {
+    :montague => {
       :patriarch => {name: "Lord Montague", age: "53"},
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => []
-   }, 
-   :capulet => {
+    },
+    :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliette", age: "15", status: "alive"},
       :heroine_friends => []
-   }
-}
+    }
+  }
 end
