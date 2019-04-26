fcdd5b1a32c36324b4907201e0e3f3100914cbca
maxwellbenton <maxwellbenton@gmail.com>
2019-04-26 16:59:26 -0400

    removing unneeded hashtags

--- spec/04_challenge_spec.rb
+++ spec/04_challenge_spec.rb
@@ -1,7 +1,7 @@
 require_relative 'spec_helper'
 require_relative '../lib/fourth_challenge.rb'
 
-describe "#fourth_challenge" do
+describe "fourth_challenge" do
 	it "fills out the Montague hero's friends array with" do
     expect(fourth_challenge[:montague][:hero_friends]).to match_array([{name: "Benvolio", age: "17", attitude: "worried"}, {name: "Mercutio", age: "18", attitude: "hot-headed"}])
 
