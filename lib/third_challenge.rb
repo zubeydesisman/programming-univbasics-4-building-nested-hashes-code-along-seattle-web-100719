944feda955b99f8e177bdcc2003fb36bc3b823bc
ZUBEYDE SISMAN <zubeydesisman@gmail.com>
2019-09-30 19:29:06 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -1,37 +1,14 @@
-def third_challenge
-  epic_tragedy = {
+epic_tragedy = {
    :montague => {
-      :patriarch => {
-        :name => "Lord Montague"
-        :age => "53"
-        },
-      :matriarch => {
-        :name => "Lady Montague"
-        :age => "54"
-        },
-      :hero => {
-        :name => "Romeo"
-        :age => "15"
-        :status => "alive"
-        },
+      :patriarch => {name: "Lord Montague", age: "53"},
+      :matriarch => {name: "Lady Montague", age: "54"},
+      :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => []
-   }, 
+   },
    :capulet => {
-      :patriarch => {
-        :name => "Lord Capulet"
-        :age => "50"
-        },
-      :matriarch => {
-        :name =>"Lady Capulet"
-        :age => "51"
-        },
-      :heroine => {
-        :name => "Juliet"
-        :age => "15"
-        :satatus => "alive"
-        },
+      :patriarch => {name: "Lord Capulet", age: "50"},
+      :matriarch => {name: "Lady Capulet", age: "51"},
+      :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => []
    }
-  }
-
-end
+}
