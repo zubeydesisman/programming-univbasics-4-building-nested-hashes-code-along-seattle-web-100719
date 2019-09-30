83ae0f35e5cc7a8939bdf941669dc07997536910
ZUBEYDE SISMAN <zubeydesisman@gmail.com>
2019-09-30 19:11:57 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,7 +1,10 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      
+      :patriarch
+      :matriarch
+      :hero
+      :hero_friends
       }, 
     :capulet => {
       #your key/value pairs here!
