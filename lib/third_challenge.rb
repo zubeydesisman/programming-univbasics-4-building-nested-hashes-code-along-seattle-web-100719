21d059d5217a24794700796c8de13538b173106b
ZUBEYDE SISMAN <zubeydesisman@gmail.com>
2019-09-30 19:28:07 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -18,13 +18,17 @@ def third_challenge
    }, 
    :capulet => {
       :patriarch => {
-        #your key/value pairs here
+        :name => "Lord Capulet"
+        :age => "50"
         },
       :matriarch => {
-        #your key/value pairs here
+        :name =>"Lady Capulet"
+        :age => "51"
         },
       :heroine => {
-        #your key/value pairs here
+        :name => "Juliet"
+        :age => "15"
+        :satatus => "alive"
         },
       :heroine_friends => []
    }
