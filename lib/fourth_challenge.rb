09a72fb901896becdf1ad64f90123c8e714c5a7e
ZUBEYDE SISMAN <zubeydesisman@gmail.com>
2019-09-30 19:35:29 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -5,7 +5,8 @@ def fourth_challenge
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
-        #your hashes here!
+       {name:"Benvolio",age: "17",attitude:"worried"},
+       {name:"Mercutio",age:"18",attitude:"hot-headed"}
       ]
    }, 
    :capulet => {
