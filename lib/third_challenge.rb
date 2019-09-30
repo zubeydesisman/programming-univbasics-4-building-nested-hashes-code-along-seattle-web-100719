9702f586996a7a0e6c2c91fca12ac6ffc5685f7a
ZUBEYDE SISMAN <zubeydesisman@gmail.com>
2019-09-30 19:25:52 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -2,13 +2,17 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {
-        #your key/value pairs here
+        :name => "Lord Montague"
+        :age => "53"
         },
       :matriarch => {
-        #your key/value pairs here
+        :name => "Lady Montague"
+        :age => "54"
         },
       :hero => {
-        #your key/value pairs here
+        :name => "Romeo"
+        :age => "15"
+        :status => "alive"
         },
       :hero_friends => []
    }, 
